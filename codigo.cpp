0e3edf39fee9ba515882d066578ee5106723c75b
AmandaGValerio <valeriogamanda@gmail.com>
2021-02-14 23:06:58 -0300

    adicionando arquivos iniciais
    adicionando os primeiros arquivos iniciais para o projeto

---
+++ codigo.cpp
@@ -0,0 +1,31 @@
+/**********************
+Autora: Amanda Gabriela Valério
+**********************/
+
+#include <iostream>
+
+using namespace std;
+
+void converterletras(){
+	//converte todas as letras para minusculo
+}
+
+int main(){
+	
+	string palavra; //palavra a ser buscada
+	string linha; //linha lida do arquivo
+	int repeticoes = 0;
+	
+	//pegando a palavra para buscar
+	cout << "Digite a palavra que você quer buscar: ";
+	cin >> palavra;
+	
+	//abre arquivo
+	
+	//se arquivo aberto com sucesso
+	cout << "Buscando..." << endl;
+	
+	//exibe a quantidade de vezes que aquela palavra foi encontrada
+	cout << "Essa palavra foi encontrada " << repeticoes << " vezes" << endl;
+	return 0;
+}
